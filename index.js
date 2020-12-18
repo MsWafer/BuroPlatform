f26aaf005f742b836fd49bf68fa77f24b73d1e3b
MsWafer <phalosof@gmail.com>
2020-12-18 14:55:50 +0300

    a

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const connectDB = require("./config/db");
+const connectDB = require("./middleware/db");
 connectDB();
 const cors = require("cors");
 require("dotenv").config();
