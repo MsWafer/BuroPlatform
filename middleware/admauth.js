6f527af940dbeb580ecb9ef249a105a76a4e99e0
MsWafer <phalosof@gmail.com>
2021-01-18 13:26:03 +0300

    a

--- middleware/admauth.js
+++ middleware/admauth.js
@@ -14,7 +14,7 @@ module.exports = function (req, res, next) {
     const decoded = jwt.verify(token, process.env.jwtSecret);
 
     req.user = decoded.user;
-    if (req.user.permission !== 'admin'){return res.status(401).json({msg:'У вас недостаточно прав для просмотра этой страницы'})}
+    if (req.user.permission == 'user' || req.user.permission == "manager"){return res.status(401).json({msg:'У вас недостаточно прав для просмотра этой страницы'})}
     next();
   } catch (err) {
     res.status(401).json({ msg: "Неверный токен авторизации" });
