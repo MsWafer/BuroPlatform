9f2b58dc6e3b69dbcc3433e14c2539d4a3f44272
MsWafer <phalosof@gmail.com>
2021-01-26 19:03:32 +0300

    a

--- middleware/rcprojcreate.js
+++ middleware/rcprojcreate.js
@@ -1,6 +1,6 @@
 const fetch = require("node-fetch");
 
-module.exports = async (title, rocketchat, rname) => 
+module.exports = async (title, rocketchat) => 
 fetch(`${process.env.CHAT}/api/v1/login`, {
   method: "post",
   headers: {
@@ -13,7 +13,6 @@ fetch(`${process.env.CHAT}/api/v1/login`, {
   }),
 })
   .then((res) => res.json())
-  // .then(()=>rname=title.replace(/:/g,""))
   .then((res) =>
     fetch(`${process.env.CHAT}/api/v1/channels.create`, {
       method: "post",
@@ -23,7 +22,7 @@ fetch(`${process.env.CHAT}/api/v1/login`, {
         "X-Auth-Token": res.data.authToken,
         "X-User-Id": res.data.userId,
       },
-      body: JSON.stringify({ name: rname }),
+      body: JSON.stringify({ name: title }),
     })
       .then((response) => response.json())
       .then((response) => rocketchat = response.channel._id)
