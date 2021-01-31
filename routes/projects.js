c813b2ee52316ea1ba7c267f65cc76bda6f99045
MsWafer <phalosof@gmail.com>
2021-01-31 15:54:02 +0300

    a

--- routes/projects.js
+++ routes/projects.js
@@ -576,8 +576,14 @@ router.post("/sprints/new/:crypt", auth, async (req, res) => {
       { crypt: req.params.crypt },
       { $push: { sprints: sprint, $position: 0 } }
     );
+    console.log(req.body)
+    await Sprint.findOneAndUpdate(
+      { dateClosePlan: req.body.date },
+      { $push: { tasks: { $each: req.body.tasks, $position: 0 } } },
+      { multi: true }
+    );
     console.log("sprint added to project");
-    return res.json({
+    return res.json({                   
       msg: `Новый спринт добавлен в проект ${sprint.dateOpen}`,
       id: sprint.id,
       tasks: sprint.tasks,
