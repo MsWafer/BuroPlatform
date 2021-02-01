54c1b9abb4231e70d905436352f8ce73cdd4fb34
MsWafer <phalosof@gmail.com>
2021-02-01 12:37:56 +0300

    a

--- routes/auth.js
+++ routes/auth.js
@@ -17,7 +17,7 @@ router.post(
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      console.log(errors.Result.errors)
+      console.log(errors.errors)
       return res.status(400).json({err:[{ err: errors.array()}] });
     }
 
