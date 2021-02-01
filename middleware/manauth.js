42fa61ea131778c3404b40a417c99909afa03e8b
MsWafer <phalosof@gmail.com>
2021-02-01 16:48:19 +0300

    a

--- middleware/manauth.js
+++ middleware/manauth.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
-module.exports = async(req, res, next)=> {
+module.exports = async (req, res, next) => {
   //get token from header
   const token = req.header("auth-token");
 
@@ -15,15 +15,15 @@ module.exports = async(req, res, next)=> {
     const decoded = jwt.verify(token, process.env.jwtSecret);
 
     req.user = decoded.user;
-    let user = await User.findOne({_id:req.user.id})
-    if (user.permission == "user") {
-      res
+    let user = await User.findOne({ _id: req.user.id });
+    if (user.permission == "user")
+      return res
         .status(401)
         .json({ msg: "У вас недостаточно прав для просмотра этой страницы" });
-    }else{
-     next(); 
-    }
-    
+
+    // else{
+    next();
+    // }
   } catch (err) {
     res.status(401).json({ msg: "Неверный токен авторизации" });
   }
