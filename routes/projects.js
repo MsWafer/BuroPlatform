18edb48bf6d3616c6311a69b28377e443e2e7f7d
MsWafer <phalosof@gmail.com>
2021-02-19 17:45:15 +0300

    a

--- routes/projects.js
+++ routes/projects.js
@@ -124,7 +124,8 @@ router.get("/",auth, async (req, res) => {
       .populate("team", "-projects -password -permission -avatar -tickets -__v")
       .populate("sprints");
     console.log(req.query)
-    let que = req.query.field;
+    let que;
+    if(req.query.order){que=1}else{que=-1}
     let order = req.query.order;
       Array.prototype.sortBy = (query) => {
         return projects.slice(0).sort(function(a,b) {
