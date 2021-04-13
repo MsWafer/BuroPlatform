741373fe34ce367eeec46c13097d2d49c356712e
MsWafer <phalosof@gmail.com>
2021-04-13 17:13:33 +0300

    a

--- routes/docs.js
+++ routes/docs.js
@@ -23,7 +23,7 @@ const storage = multer.diskStorage({
 });
 const upload = multer({
   storage: storage,
-  limits: { fileSize: 3 * 1024 * 1024 },
+  limits: { fileSize: 5 * 1024 * 1024 },
   fileFilter: (req, file, cb) => {
     if (
       file.mimetype == "image/png" ||
--- routes/mobroutes.js
+++ routes/mobroutes.js
@@ -102,4 +102,109 @@ router.get("/mobprjspr/:crypt", async (req, res) => {
   }
 });
 
+//join v2.0
+router.put("/join2/:crypt", auth, async (req, res) => {
+  try {
+    //checks
+    let project = await Project.findOne({ crypt: req.params.crypt }).populate(
+      "team2.user",
+      "_id"
+    );
+    if (!project) {
+      return res.status(404).json({ err: "Проект не найден" });
+    }
+    let user = await User.findOne({ _id: req.user.id });
+    let member_object;
+
+    let team_check = project.team2.filter(
+      (userObj) => userObj.user._id.toString() === user._id.toString()
+    );
+
+    if (team_check.length == 0) {
+      //join team
+      member_object = {
+        position: req.body.position,
+        task: req.body.task,
+        user: req.user.id,
+      };
+      project.team2.push(member_object);
+      user.projects.push(project._id);
+
+      if (project.rocketchat) {
+        await rcinvprj(project, user);
+      }
+    } else {
+      //leave team
+      project.team2 = project.team2.filter(
+        (user_obj) => user_obj.user._id != req.user.id
+      );
+      user.projects = user.projects.filter(
+        (prj) => prj != project._id.toString()
+      );
+      if (project.rocketchat) {
+        await rckickprj(project, user);
+      }
+    }
+    await project.save();
+    await user.save();
+    res.redirect(303, `/mob/mobteam2/${project.crypt}/`);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ err: "server error" });
+  }
+});
+
+//deactivate task
+router.put("/sprints/DAtask/:taskid", auth, async (req, res) => {
+  try {
+    let sprint = await Sprint.findOne({
+      "tasks._id": req.params.taskid,
+    });
+    if (!sprint) {
+      return res.status(404).json({ msg: "Спринт не найден" });
+    }
+    let task = sprint.tasks.filter((el) => {
+      el._id == req.params.taskid;
+    })[0];
+    task.taskStatus = !task.taskStatus;
+    let num = task.taskStatus ? 1 : -1;
+    let d = new Date();
+    await Stat.findOneAndUpdate(
+      {
+        day: d.getDate(),
+        month: d.getMonth() + 1,
+        year: d.getFullYear(),
+      },
+      { $inc: { task_close_count: num } }
+    );
+    await sprint.save();
+    await Sprint.populate(sprint, "tasks.user");
+    return res.json(task);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ err: "server error" });
+  }
+});
+
+//edit task
+router.put("/sprints/taskedit/:taskid", auth, async (req, res) => {
+  try {
+    let sprint = await Sprint.findOne({"tasks._id": req.params.taskid }).populate(
+      "creator"
+    );
+    if (!sprint) {
+      return res.status(404).json({ err: "Спринт не найден" });
+    }
+    let a = sprint.tasks.filter((task) => task._id == req.params.taskid)[0];
+    let keys = Object.keys(req.body);
+    keys.forEach((key) => (a[key] = req.body[key]));
+    await sprint.save();
+    await Sprint.populate(sprint, "tasks.user");
+    return res.json(a);
+  } catch (error) {
+    console.error(error);
+    return res.json({ msg: "server error" });
+  }
+});
+
 module.exports = router;
